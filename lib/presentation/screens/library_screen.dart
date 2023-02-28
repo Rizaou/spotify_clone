16e1cd593830bc80eb7f9cf6a90df3790f6f03cb
Deniz Basocak <denizzr@outlook.com>
2023-02-28 11:19:53 +0300

    FadeTransition added library scene

--- lib/presentation/screens/library_screen.dart
+++ lib/presentation/screens/library_screen.dart
@@ -4,72 +4,101 @@ import 'package:spotify_clone/presentation/custom-theme.dart';
 import 'package:spotify_clone/presentation/widgets/custom_appbar.dart';
 import 'package:spotify_clone/presentation/widgets/library_items.dart';
 
-class LibraryScreen extends StatelessWidget {
-  const LibraryScreen({super.key});
+class LibraryScreen extends StatefulWidget {
+  LibraryScreen({super.key});
+
+  @override
+  State<LibraryScreen> createState() => _LibraryScreenState();
+}
+
+class _LibraryScreenState extends State<LibraryScreen>
+    with TickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 400));
+    _animation = Tween(begin: 0.0, end: 1.0).animate(_controller);
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _controller.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
+    _controller.forward();
     return SafeArea(
-      child: Column(
-        children: [
-          Container(
-            color: bgColor,
-            height: kToolbarHeight * 2,
-            width: double.infinity,
-            child: CustomAppBar(),
-          ),
-          Expanded(
-            child: NotificationListener<OverscrollIndicatorNotification>(
-              onNotification: (notification) {
-                notification.disallowIndicator();
-                return notification.accepted;
-              },
-              child: ListView(
-                shrinkWrap: true,
-                children: [
-                  LibraryItems(
-                    title: "Liked Songs",
-                    subtitle: "Playlist . 41 songs",
-                    color: Colors.blue,
-                  ),
-                  LibraryItems(
-                    title: "New Episodes",
-                    subtitle: "Updated Yesterday",
-                    color: Colors.purple,
-                  ),
-                  LibraryItems(
-                    title: "Türkçe",
-                    subtitle: "Playlist * Deniz",
-                  ),
-                  LibraryItems(
-                    title: "Türkçe",
-                    subtitle: "Playlist * Deniz",
-                  ),
-                  LibraryItems(
-                    title: "Türkçe",
-                    subtitle: "Playlist * Deniz",
-                  ),
-                  LibraryItems(
-                    title: "Türkçe",
-                    subtitle: "Playlist * Deniz",
-                  ),
-                  LibraryItems(
-                    title: "Türkçe",
-                    subtitle: "Playlist * Deniz",
-                  ),
-                  LibraryItems(
-                    title: "Türkçe",
-                    subtitle: "Playlist * Deniz",
-                  ),
-                  LibraryItems(
-                    title: "Türkçe",
-                    subtitle: "Playlist * Deniz",
-                  ),
-                ],
+      child: FadeTransition(
+        opacity: _animation,
+        child: Column(
+          children: [
+            Container(
+              color: bgColor,
+              height: kToolbarHeight * 2,
+              width: double.infinity,
+              child: CustomAppBar(),
+            ),
+            Expanded(
+              child: NotificationListener<OverscrollIndicatorNotification>(
+                onNotification: (notification) {
+                  notification.disallowIndicator();
+                  return notification.accepted;
+                },
+                child: ListView(
+                  shrinkWrap: true,
+                  children: [
+                    LibraryItems(
+                      title: "Liked Songs",
+                      subtitle: "Playlist . 41 songs",
+                      color: Colors.blue,
+                    ),
+                    LibraryItems(
+                      title: "New Episodes",
+                      subtitle: "Updated Yesterday",
+                      color: Colors.purple,
+                    ),
+                    LibraryItems(
+                      title: "Türkçe",
+                      subtitle: "Playlist * Deniz",
+                    ),
+                    LibraryItems(
+                      title: "Türkçe",
+                      subtitle: "Playlist * Deniz",
+                    ),
+                    LibraryItems(
+                      title: "Türkçe",
+                      subtitle: "Playlist * Deniz",
+                    ),
+                    LibraryItems(
+                      title: "Türkçe",
+                      subtitle: "Playlist * Deniz",
+                    ),
+                    LibraryItems(
+                      title: "Türkçe",
+                      subtitle: "Playlist * Deniz",
+                    ),
+                    LibraryItems(
+                      title: "Türkçe",
+                      subtitle: "Playlist * Deniz",
+                    ),
+                    LibraryItems(
+                      title: "Türkçe",
+                      subtitle: "Playlist * Deniz",
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
